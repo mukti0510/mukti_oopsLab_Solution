604282f6e5348165918f185ac82f755c60f34ec8
mukti0510 <107801960+mukti0510@users.noreply.github.com>
2022-07-31 21:05:13 +0530

    Add files via upload

---
+++ Mukti_Bhargava_DataStructure_Assignment_Solution/src/com/greatlearning/dataStructures/Question1/SkyScraperMain.java
@@ -0,0 +1,15 @@
+package com.greatlearning.dataStructures.Question1;
+
+public class SkyScraperMain {
+
+	public static void main(String[] args) {
+		
+		// Constructed Floor Input from user
+		FloorConstruction.floorConstructionInput();
+		
+		// Display Assemble Plan 
+		FloorConstruction.dispalyAssemblePlan();
+		
+	}
+
+}
---
+++ Mukti_Bhargava_DataStructure_Assignment_Solution/src/com/greatlearning/dataStructures/Question2/Node.java
@@ -0,0 +1,13 @@
+package com.greatlearning.dataStructures.Question2;
+
+class Node {
+	
+	int value;
+	Node left, right;
+	
+	public Node (int value) {
+		this.value = value;
+		left = null;
+		right = null;
+	}
+}
